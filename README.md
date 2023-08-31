7deb00cf993c373355b8124a8d84c2b4cdcbcfd4
Paul OGGERO <paul.oggero@itgrate.com>
2023-08-31 19:00:57 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CalisteSwift
+
+A description of this package.
---
+++ Tests/CalisteSwiftTests/CalisteSwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CalisteSwift
+
+final class CalisteSwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CalisteSwift().text, "Hello, World!")
+    }
+}
