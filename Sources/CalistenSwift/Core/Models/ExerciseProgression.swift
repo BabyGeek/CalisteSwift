b1ab20068e72a3adef654c099c2443dd10fbde1e
PaulOgg <paul.oggero@itgratevn.com>
2024-08-27 10:27:02 +0700

    ♻️ Add progression and regression models

--- Sources/CalistenSwift/Core/Models/ExerciseProgression.swift
+++ Sources/CalistenSwift/Core/Models/ExerciseProgression.swift
@@ -1,8 +1,13 @@
 //
-//  File.swift
-//  
+//  ExerciseProgression.swift
+//
 //
 //  Created by Paul Oggero on 25/8/24.
 //
 
 import Foundation
+
+public struct ExerciseProgression {
+    let order: Int
+    let exerciseName: ExerciseName
+}
--- Sources/CalistenSwift/Core/Models/ExerciseRegression.swift
+++ Sources/CalistenSwift/Core/Models/ExerciseRegression.swift
@@ -1,8 +1,13 @@
 //
-//  File.swift
-//  
+//  ExerciseRegression.swift
+//
 //
 //  Created by Paul Oggero on 25/8/24.
 //
 
 import Foundation
+
+public struct ExerciseRegression {
+    let order: Int
+    let exerciseName: ExerciseName
+}
