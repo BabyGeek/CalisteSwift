e09b84da940058a6a1e25cc2494580d5920b5a88
PaulOgg <paul.oggero@itgratevn.com>
2024-08-27 10:27:37 +0700

    ♻️ Add base protocol

--- Sources/CalistenSwift/Core/Protocols/Exercisable.swift
+++ Sources/CalistenSwift/Core/Protocols/Exercisable.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+    /// Protocol that defines the common properties and methods for all exercises.
+public protocol Exerciseable {
+    var name: ExerciseName { get }
+    var muscleGroup: MuscleGroup { get }
+    var targetedMuscles: [Muscle] { get }
+    var difficulty: Difficulty { get }
+    var types: [ExerciseType] { get }
+    var routines: [Routine] { get }
+    var side: Side { get }
+    var force: Force { get }
+    var requiredEquipments: [Equipment] { get }
+    var optionalEquipments: [Equipment] { get }
+    var direction: Direction { get }
+    var categories: [Category] { get }
+    var variations: [ExerciseName] { get }
+    var progressions: [ExerciseProgression] { get }
+    var regressions: [ExerciseRegression] { get }
+}
